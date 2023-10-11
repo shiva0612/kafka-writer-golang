67ac3269b34725b0b2139c8336749068ec6707a1
Shiva <36275739+shiva0612@users.noreply.github.com>
2023-10-11 05:21:26 +0000

    workerpool added

--- workerPool.go
+++ workerPool.go
@@ -1,1 +1,26 @@
 package main
+
+type workerPool struct {
+	maxWorker   int
+	queuedTaskC chan func()
+}
+
+func NewWorkerPool(poolSize int) *workerPool {
+	wp := workerPool{maxWorker: poolSize}
+	wp.queuedTaskC = make(chan func(), 1000)
+	return &wp
+}
+
+func (wp *workerPool) Run() {
+	for i := 0; i < wp.maxWorker; i++ {
+		go func() {
+			for task := range wp.queuedTaskC {
+				task()
+			}
+		}()
+	}
+}
+
+func (wp *workerPool) AddTask(task func()) {
+	wp.queuedTaskC <- task
+}
